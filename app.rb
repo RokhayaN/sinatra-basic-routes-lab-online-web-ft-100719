0cf3e35de7f98de7b422bd7a90055dc943135c6c
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-28 20:18:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,15 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get name '/' do
+  get 'name/' do
     "My name is Rokhaya"
  end
 
-  get hometown'/'do
+  get 'hometown/'do
     "My hometown is Dakar "
   end
   
-  get favorite song'/'do
+  get 'favorite song/'do
     "My favorite song is joro wizkid"
   end
 end
